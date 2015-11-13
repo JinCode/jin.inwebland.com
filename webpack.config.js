69d1516d58a55f925d5959595661d57e728dbb3c
JinCode <jincoding@gmail.com>
2015-11-12 22:45:16 -0500

    minified bundle

--- webpack.config.js
+++ webpack.config.js
@@ -7,42 +7,47 @@ var APP = __dirname;
 module.exports = {
     // config goes here
     module:{
-        loaders: [
-            { test: /\.scss$/, loader: "style!css!sass"},
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            { test: /\.jsx$/, loader: 'babel-loader'},
-            { test: /\.png$/, loader: 'url-loader'}
-        ]
+      loaders: [
+      { test: /\.scss$/, loader: "style!css!sass"},
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      { test: /\.jsx$/, loader: 'babel-loader'},
+      { test: /\.png$/, loader: 'url-loader'}
+      ]
     },
 
     context: ORIGIN,
     entry: {
-        app: [
-            'webpack/hot/dev-server',
-            './entry.js'
-        ]
+      app: [
+      'webpack/hot/dev-server',
+      './entry.js'
+      ]
     },
     output: {
-        path: APP,
-        filename: 'bundle.js'
+      path: APP,
+      filename: 'bundle.js'
     },
 
     devServer: {
-        port: 1991,
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true
+      port: 1991,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
     ],
     externals: {
         //don't bundle the 'react' npm package with our bundle.js
         //but get it from a global 'React' variable
         'react': 'React'
-    },
-    resolve: {
+      },
+      resolve: {
         extensions: ['', '.js', '.jsx']
-    }
-};
+      }
+    };
